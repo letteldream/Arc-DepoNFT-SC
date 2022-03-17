1ea62b8747912bb272ae92c068def12adb4e262f
letteldream <letteldream@gmail.com>
2022-03-16 19:04:21 -0500

    create Arc721

---
+++ contracts/Arc721.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/**
+ * @title Arc721
+ * Arc721 - ERC721 contract that has minting functionality.
+ */
+contract Arc721 is ERC721, ERC721Enumerable, ERC721URIStorage {
+    event Minted(
+        uint256 indexed tokenId,
+        address indexed minter,
+        string tokenUri
+    );
+
+    uint256 private _currentTokenId = 0;
+
+    /// @notice Contract constructor
+    constructor() ERC721("Arc721", "ARC721") {}
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _tokenUri link to metadata
+     */
+    function mint(string calldata _tokenUri) external {
+        uint256 newTokenId = _getNextTokenId();
+        _safeMint(_msgSender(), newTokenId);
+        _setTokenURI(newTokenId, _tokenUri);
+        _incrementTokenId();
+
+        emit Minted(newTokenId, _msgSender(), _tokenUri);
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId + 1;
+    }
+
+    /**
+     * @dev increments the value of _currentTokenId
+     */
+    function _incrementTokenId() private {
+        _currentTokenId++;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    /**
+     * @dev Hook that is called before any token transfer. This includes minting
+     * and burning.
+     *
+     * Calling conditions:
+     *
+     * - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be
+     * transferred to `to`.
+     * - When `from` is zero, `tokenId` will be minted for `to`.
+     * - When `to` is zero, ``from``'s `tokenId` will be burned.
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev Destroys `tokenId`.
+     * The approval is cleared when the token is burned.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
